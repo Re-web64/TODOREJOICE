d45982e832fef9f515049c4d878a7371b3254cef
Re-web64 <144208384+Re-web64@users.noreply.github.com>
2023-10-05 05:33:55 -0700

    Added three files

---
+++ todo.js
@@ -0,0 +1,86 @@
+const ul = document. getElementById("fat")
+const de=document.getElementById("deleteMe")
+//const input = document. getElementById("todo")
+//const addbutton = document. getElementById("addButton")
+//const todo = document. getElementById("todoList")
+
+
+
+function can(){
+
+//for (let i = 0; i < newtask.length; i++) {
+
+//if (document.getElementById("newtask input")) {
+//	alert("kINDLY ENTER YOUR TODO")
+
+//}
+//else{
+ document.getElementById("tasks").innerHTML +=
+ `<div class="tasks"> 
+${document.getElementById("Todo").value}
+<button class="delete">delete</button>
+  </div>
+  `
+
+//}
+
+//}
+}
+
+//i just updated my JS file
+function can1(){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let todoitems=[]
+//let counter = 0
+
+//function addtodo(){
+
+//}
+
+
+//let num1 = 1;
+//while(num1 != 5){
+	//num1=prompt("1:Add todo,2:View todo, 3:Edit todo, 4:Delete todo, 5:Exist")
+//if (num1==1){
+		// alert("please add your today tasks")
+//let todo=prompt("input your tasks now!!!")
+//todoitems.push(todo)
+//console.log(todoitems)
+
+//}
+//else if(num1 == 2){
+	//console.log(todoitems)
+//}
+//else if(num1 == 3){
+	// let work=prompt("enter the index number you wish to edit")
+//let olditem=prompt("enter the item you want to remove")
+//let newitem=prompt("enter what you want to replace it with")	
+	// todoitems[work]=num1;
+//let ind=todoitems.indexOf(olditem)
+//console.log(ind)
+//if(ind==-1){
+	//console.log("The item is not here")
+//}
+//else {
+//todo[ind]=new item
+//}
+//}
+
+//}
